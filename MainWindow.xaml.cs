99f6613f966f8496cafd498e21b0b1d00a95cfd5
Luke Storry <luke.storry@ghyston.com>
2020-05-07 13:50:49 +0100

    got iframe and click handlers working

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,17 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Text.Json.Serialization;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
+using System.Windows.Controls.Primitives;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using Microsoft.Maps.MapControl.WPF;
 
 namespace WPFMapControlPrototype
@@ -28,6 +22,80 @@ namespace WPFMapControlPrototype
                 new ApplicationIdCredentialsProvider(Environment.GetEnvironmentVariable("BING_MAPS_KEY"));
             myMap.Center = new Location(51.444, -2.604);
             myMap.ZoomLevel = 14;
+            // myMap.MouseLeftButtonDown += (sender, e) =>
+            // {
+            //     myMap.ScreenToGeoPoint(e.GetPosition(this.mapControl1))
+            //     Location l = new Location();
+            //     myMap.mouTryPixelToLocation(e.GetCurrentPoint(myMap).Position, out l);
+            //     Console.WriteLine(sender.GetType());
+            // };
+
+
+            var mapItems = new[]
+            {
+                new MapItem
+                {
+                    Location = new Location(51.45, -2.59),
+                    Title = "Location 1",
+                    Details = "First Location Details   \n     would \n      go \n       here.         ",
+                    Uri = new Uri("https://example.com"),
+                    Color = Colors.Green
+                },
+                new MapItem
+                {
+                    Location = new Location(51.44, -2.595),
+                    Title = "Second Location",
+                    Details = "Details   \n     would \n      go \n       here.         ",
+                    Uri = new Uri("https://google.com"),
+                    Color = Colors.Blue
+                }
+            };
+
+            foreach (var mapItem in mapItems)
+            {
+                var poly = new MapPolygon
+                {
+                    Stroke = new SolidColorBrush(mapItem.Color),
+                    StrokeThickness = 20,
+                    Locations = new LocationCollection {mapItem.Location, mapItem.Location},
+                    Tag = mapItem,
+                    ToolTip = mapItem.Title
+                };
+                poly.MouseLeftButtonDown += DetailsPopup;
+                poly.MouseRightButtonDown += SetIframeSrc;
+                myMap.Children.Add(poly);
+            }
+        }
+
+        private class MapItem
+        {
+            public Location Location;
+            public string Title;
+            public string Details;
+            public Uri Uri;
+            public Color Color;
+        }
+
+        private void SetIframeSrc(object sender, RoutedEventArgs e)
+        {
+            if (sender is MapPolygon poly)
+            {
+                if (poly.Tag != null && poly.Tag is MapItem item)
+                {
+                    webBrowser.Source = item.Uri;
+                }
+            }
+        }
+
+        private static void DetailsPopup(object sender, RoutedEventArgs e)
+        {
+            if (sender is MapPolygon poly)
+            {
+                if (poly.Tag != null && poly.Tag is MapItem item)
+                {
+                    MessageBox.Show(item.Details, $"{item.Title} Status");
+                }
+            }
         }
     }
 }
